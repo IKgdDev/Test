580759fe86be90cf43b3d314bfe799f81791ca2d
Aleksandr Desyatnichenko <alik_kenig@rambler.ru>
2022-08-30 22:01:52 +0200

    Added method pick

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,5 +1,7 @@
+import java.util.Random;
+
 public class MagicBox<T> {
-    T[] items;
+    private T[] items;
 
     public MagicBox(int itemsCount) {
         items = (T[]) new Object[itemsCount];
@@ -14,4 +16,17 @@ public class MagicBox<T> {
         }
         return false;
     }
+
+    public T pick() throws RuntimeException {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null) {
+                throw new RuntimeException("Коробка не полна, осталось заполнить ещё " + (items.length - i));
+            }
+        }
+        Random random = new Random();
+        int randomInt = random.nextInt(items.length);
+
+        return items[randomInt];
+    }
+
 }
